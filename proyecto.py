0906b56df2a661a605a488f88ea09c9682c5ea24
axelgolin4 <axelgolin4@gmail.com>
2022-09-22 00:23:29 -0600

    first commit

---
+++ proyecto.py
@@ -0,0 +1,53 @@
+import os
+
+bandera = 1
+print("Bienvenido a nuestro interprete de comandos")
+print("")
+input("Precione enter para continuar...")
+os.system("clear")
+
+while(bandera == 1):
+        os.system("pwd")
+        a = input()
+
+        if a == "pwd":
+                os.system("pwd")
+        elif a == "date":
+                os.system("date")
+        elif a == "time":
+                os.system("time")
+        elif a == "exit":
+                bandera = 2
+        elif a == "clear":
+                os.system("clear")
+        elif a == "Man":
+                print("pwd Muestra el directorio activo")
+                print("time muestra/establece la hora.")
+                print("date muestra/establece la fecha.")
+                print("exit sale del intérprete ó programa.-")
+                print("")
+                print("")
+        elif a == "uname -a":
+                os.system("ver")
+
+        #El CD NO FUNCIONA
+        elif a == "cd":
+                os.system("cd ..")
+
+        elif a == "ls":
+                os.system("ls")
+        elif a[0:5] == "mkdir":
+                b = "mkdir "+ str(a[6:])
+                os.system(b) 
+        elif a[0:5] == "rmdir":
+                b = "rmdir "+ str(a[6:])
+                os.system(b) 
+        elif a[0:2] == "rm":
+                b = "rm "+ str(a[3:])
+                os.system(b)                     
+        else:
+                print("Comando equivocado")
+
+        print("")
+     
+        
